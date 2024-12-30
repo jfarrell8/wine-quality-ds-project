e4deaefa30277ef69bfef47ad0e78e6726483f11
John Farrell <johndfarrell@gmail.com>
2024-12-29 22:17:06 -0600

    added data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.wine_quality_ds_project import logger
+from src.wine_quality_ds_project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging data science")
+STAGE_NAME="Data Ingestion Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
