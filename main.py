1f9369cd147f761cb5c307ccd4f7daded7dc3362
John Farrell <johndfarrell@gmail.com>
2024-12-30 08:20:49 -0600

    model trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.wine_quality_ds_project import logger
 from src.wine_quality_ds_project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.wine_quality_ds_project.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.wine_quality_ds_project.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.wine_quality_ds_project.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 try:
@@ -31,6 +32,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.initiate_model_training()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
